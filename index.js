1bb5d285dbe25cddf3a4957dd0897d041ede5a43
toonajuig <thirawat.devtt@gmail.com>
2026-04-08 15:11:54 +0700

    edit-conflict

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Student = require("./school/Student");
 const Teacher = require("./school/Teacher");
+const Course = require("./school/Course");
+const Classroom = require("./school/classroom");
+const Enrollment = require("./school/Enrollment");
+const Grade = require("./school/Grade");
+const Schedule = require("./school/Schedule");
 
 const student1 = new Student("S001", "Somchai", 15, "Grade 10");
 const teacher1 = new Teacher("T001", "Anan", 35, "Math");
